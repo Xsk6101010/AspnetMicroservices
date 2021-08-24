afae32bff099e699d84960933db9b2350d463c15
TienNV25 DEV03 FPTSoft VietName <hjhjcallme1@gmail.com>
2021-08-25 06:14:19 +0700

    37. develop repository pattern.

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> Get(string name);
+        Task<ShoppingCart> Update(ShoppingCart basket);
+
+        Task Delete(string name);
+    }
+}
