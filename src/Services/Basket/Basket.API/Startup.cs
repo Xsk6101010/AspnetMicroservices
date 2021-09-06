56b5b28bc1f762edff1cb7ed6e9d3944a814ba7b
TienNV25 DEV03 FPTSoft VietName <hjhjcallme1@gmail.com>
2021-09-06 15:11:40 +0700

    72.  Register Discount Grpc Client and Discount Grpc Service into the Basket.API

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -10,7 +10,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 
 namespace Basket.API
 {
@@ -29,6 +31,10 @@ namespace Basket.API
             services.AddStackExchangeRedisCache(options =>
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString"));
             services.AddScoped<IBasketRepository, BasketRepository>();
+
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            services.AddScoped<DiscountGrpcService>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
