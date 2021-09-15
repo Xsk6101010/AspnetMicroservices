297f1d860531af19ab0805ffa33065d2ead5dc50
TienNV25 DEV03 FPTSoft VietName <hjhjcallme1@gmail.com>
2021-09-15 15:53:37 +0700

    104. Register Application and Infrastructure into Ordering.API

--- src/Service/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
+++ src/Service/Ordering/Ordering.Infrastructure/InfrastructureServiceRegistration.cs
@@ -12,17 +12,14 @@ namespace Ordering.Infrastructure
 {
     public static class InfrastructureServiceRegistration
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddDbContext<OrderContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
-
             services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
             services.AddScoped<IOrderRepository, OrderRepository>();
-
             services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
             services.AddTransient<IEmailService, EmailService>();
-
             return services;
         }
     }
