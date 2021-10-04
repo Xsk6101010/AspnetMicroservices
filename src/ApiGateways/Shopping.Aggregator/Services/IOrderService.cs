489750ee01f66e5e5cce11bb2be070098ce8e0da
TienNV25 DEV03 FPTSoft VietName <hjhjcallme1@gmail.com>
2021-10-04 10:24:10 +0700

    150.Developing Service Classes for Consuming Internal Microservices in Shopping.Aggr

---
+++ src/ApiGateways/Shopping.Aggregator/Services/IOrderService.cs
@@ -0,0 +1,11 @@
+﻿using Shopping.Aggregator.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    interface IOrderService
+    {
+        Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName);
+    }
+}
